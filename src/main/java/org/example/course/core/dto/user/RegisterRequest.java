ca8b826f3c22dc3956776ab50c8814c642f676e7
Cahid Hüseyn <106091116+jahidhuseyn@users.noreply.github.com>
2025-02-20 17:57:18 +0400

    Create RegisterRequest.java

---
+++ src/main/java/org/example/course/core/dto/user/RegisterRequest.java
@@ -0,0 +1,17 @@
+package org.example.course.core.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = lombok.AccessLevel.PRIVATE)
+public class RegisterRequest {
+
+    String username;
+    String password;
+
+}

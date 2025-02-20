7b9478926f3068b624562b87a3b7655825eaa8cc
Cahid Hüseyn <106091116+jahidhuseyn@users.noreply.github.com>
2025-02-21 00:35:35 +0400

    Create UserReadResponse.java

---
+++ src/main/java/org/example/course/core/entity/user/dto/UserReadResponse.java
@@ -0,0 +1,16 @@
+package org.example.course.core.entity.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@FieldDefaults(level = lombok.AccessLevel.PRIVATE)
+public class UserReadResponse {
+    String username;
+}

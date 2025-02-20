7307f208e4445e75b9fe943155d494a13f72293f
Cahid Hüseyn <106091116+jahidhuseyn@users.noreply.github.com>
2025-02-20 17:57:51 +0400

    Create JwtConfig.java

---
+++ src/main/java/org/example/course/utility/jwt/JwtConfig.java
@@ -0,0 +1,15 @@
+package org.example.course.utility.jwt;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "jwt")
+public class JwtConfig {
+    private String secret;
+}
+

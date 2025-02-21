cdf6f9bd3b63f4c80eb487223bdce98858f6fc88
Cahid Hüseyn <106091116+jahidhuseyn@users.noreply.github.com>
2025-02-21 12:56:09 +0400

    Update StudentMapper.java

--- src/main/java/org/example/course/erp/students/mapper/StudentMapper.java
+++ src/main/java/org/example/course/erp/students/mapper/StudentMapper.java
@@ -2,8 +2,11 @@ package org.example.course.erp.students.mapper;
 
 import org.example.course.core.dto.user.EntityById;
 import org.example.course.erp.students.dto.StudentCreateRequest;
+import org.example.course.erp.students.dto.StudentUpdateRequest;
 import org.example.course.erp.students.entity.StudentEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface StudentMapper {
@@ -11,4 +14,7 @@ public interface StudentMapper {
     StudentEntity toEntity(StudentCreateRequest request);
 
     EntityById toEntityById(StudentEntity entity);
+
+    @Mapping(target = "id", ignore = true)
+    StudentEntity studentUpdateDtoToStudent(@MappingTarget StudentEntity student, StudentUpdateRequest dto);
 }

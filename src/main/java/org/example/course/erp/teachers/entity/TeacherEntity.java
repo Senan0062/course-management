540604f81335dfca12ca1b05fb880234b64afb96
Cahid Hüseyn <106091116+jahidhuseyn@users.noreply.github.com>
2025-02-21 12:56:21 +0400

    Create TeacherEntity.java

---
+++ src/main/java/org/example/course/erp/teachers/entity/TeacherEntity.java
@@ -0,0 +1,43 @@
+package org.example.course.erp.teachers.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "teachers")
+public class TeacherEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    Long id;
+    @Column(name = "created_date")
+    LocalDateTime createdDate;
+    @Column(name = "modification_date")
+    LocalDateTime modificationDate;
+    @Column(name = "first_name")
+    String firstName;
+    @Column(name = "last_name")
+    String lastName;
+    @Column(name = "email")
+    String email;
+    @Column(name = "birth_date")
+    LocalDate birthDate;
+    @Column(name = "status")
+    String status;
+    @Column(name = "speciality")
+    String speciality;
+    @Column(name = "teaching_hours")
+    Long teachingHours;
+    @Column(name = "gender")
+    Long gender;
+    @Column(name = "phone_number")
+    Long phoneNumber;
+}

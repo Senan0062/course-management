7dd5a163162ed49bce62f36de67d6aa630ea3eaf
Cahid Hüseyn <106091116+jahidhuseyn@users.noreply.github.com>
2025-02-21 11:37:46 +0400

    Update StudentMapper.java

--- src/main/java/org/example/course/erp/students/mapper/StudentMapper.java
+++ src/main/java/org/example/course/erp/students/mapper/StudentMapper.java
@@ -1,8 +1,14 @@
 package org.example.course.erp.students.mapper;
 
+import org.example.course.core.dto.user.EntityById;
+import org.example.course.erp.students.dto.StudentCreateRequest;
+import org.example.course.erp.students.entity.StudentEntity;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface StudentMapper {
 
+    StudentEntity toEntity(StudentCreateRequest request);
+
+    EntityById toEntityById(StudentEntity entity);
 }

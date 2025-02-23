e1ea635f0225c792677a13a6d29b2d00ab7325c5
Cahid Hüseyn <106091116+jahidhuseyn@users.noreply.github.com>
2025-02-24 03:53:42 +0400

    Create WebMvcConfig.java

---
+++ src/main/java/org/example/course/core/service/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package org.example.course.core.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private RequestLoggingInterceptor requestLoggingInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(requestLoggingInterceptor);
+    }
+}

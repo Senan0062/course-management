b4e0a087c292bebe612262af02f1bc4bec295854
senanqurbanov <sgurbanov@infra.dc>
2025-02-23 04:07:42 +0400

    Security problem solved

--- src/main/java/org/example/course/core/entity/user/CustomUserDetailsService.java
+++ src/main/java/org/example/course/core/entity/user/CustomUserDetailsService.java
@@ -1,10 +1,8 @@
-package org.example.course.core.service;
-
-
-import java.util.Collections;
-import lombok.RequiredArgsConstructor;
+package org.example.course.core.entity.user;
 import org.example.course.core.entity.user.User;
 import org.example.course.core.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -12,19 +10,19 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class UserDetailsServiceImpl implements UserDetailsService {
-
+@Primary
+public class CustomUserDetailsService implements UserDetailsService{
     private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username)
+        return userRepository.findByUsername(username)
+                .map(user -> org.springframework.security.core.userdetails.User
+                        .withUsername(user.getUsername())
+                        .password(user.getPassword())  // Hash'lenmiş şifreyi kullanıyoruz
+                        .roles(user.getRole().name())  // Kullanıcı rolü atanıyor
+                        .build()
+                )
                 .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
-
-        return new org.springframework.security.core.userdetails.User(
-                user.getUsername(),
-                user.getPassword(),
-                Collections.emptyList()
-        );
     }
 }
